dff00af285baec50874969373a98c9550301d2b2
WolfhoundAD <Loners14@yandex.ru>
2024-02-24 16:32:34 +0300

    Add plugins in pom.xml

--- src/main/java/api/mapper/CustomerMapper.java
+++ src/main/java/api/mapper/CustomerMapper.java
@@ -2,10 +2,19 @@ package api.mapper;
 import api.dto.CustomerDTO;
 import api.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 @Mapper
 @Component
 public interface CustomerMapper {
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "address", target = "address")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phone", target = "phone")
     CustomerDTO customerToCustomerDTO(Customer customer);
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "address", target = "address")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phone", target = "phone")
     Customer customerDTOToCustomer(CustomerDTO customerDTO);
 }
